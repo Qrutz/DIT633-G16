3fb913917cb8944d75a9ddb25d3d62ad527cbdae
Joey Karlsson <joeyk@net.chalmers.se>
2024-01-29 19:56:21 +0100

    updates to take rotation from argument

--- WP1/Joey/part2.c
+++ WP1/Joey/part2.c
@@ -3,8 +3,36 @@
 // Exercise 2
 // Submission code: XXXX (provided by your TA-s)
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    int rotation;
+    char *check;
+    //check arguments are correct
+    if (argc != 2) {
+        printf("Error: Invalid number of arguments. Please enter only 1 argument, the encryption offset.");
+        return 2;
+    }
+
+    //check that string value is a number
+    check = argv[1];
+    while (*check) {
+        //check if the value at the current position is not a number
+        if (isdigit(*check++) == 0) {
+            printf("Error: Invalid number.");
+            return 2;
+        }
+    } 
+
+    //check that value is between 1 and 25
+    rotation = atoi(argv[1]);
+    if (rotation < 1 || rotation > 25) {
+        printf("%d", rotation);
+        printf("Error: Invalid number. Please enter an integer between 1 and 25");
+        return 2;
+    }
+
     //will run until an EOF is entered
     while (1) {
         //stores the plaintext string
@@ -17,17 +45,21 @@ int main() {
             return 0;
         }
 
-        //performs rot13 encryption
+        //performs caesar cipher encryption according to argument input
         for (int i = 0; plaintext[i] != '\0'; i++) {
-            //subtracts 13 if the letter is n - z
-            if ((plaintext[i] >= 'n' && plaintext[i] <= 'z') || (plaintext[i] >= 'N' && plaintext[i] <= 'Z')) {
-                plaintext[i] -= 13;
-            //adds 13 if the letter is a - m
-            } else if ((plaintext[i] >= 'a' && plaintext[i] <= 'm') || (plaintext[i] >= 'A' && plaintext[i] <= 'M')) {
-                plaintext[i] += 13;
+            int temp = (int)plaintext[i];
+            if (temp >= 65 && temp <= 90) {
+                temp += rotation - 65;
+                temp %= 26;
+                printf("%d", temp + 65);
+            } else if (temp >= 97 && temp <= 122) {
+                temp += rotation - 97;
+                temp %= 26;
+                printf("%c", temp + 97);
+            } else {
+                printf("%c", plaintext[i]);
             }
         }
-
-        printf("%s\n", plaintext);
+        printf("\n");
     }
 }
