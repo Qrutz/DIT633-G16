e4fb745cff6fffc566fe12f6587c9d792823f00e
Johan Sandgren <gussanjoba@student.gu.se>
2024-01-29 18:54:23 +0100

    4A error handling

--- WP1/Johan/4dec2bin.c
+++ WP1/Johan/4dec2bin.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h> // so we can we atoi.
+#include <ctype.h>  // so we can use isdigit
 
 int calculateBitsNeeded(unsigned int number)
 {
+    // 8 bits needed for decimals in range [1-225]
     if (number >= 1 && number <= 255)
     {
         return 8;
     }
+    // 16 bits needed for decimals in range [256-65535]
     else if (number > 255 && number <= 65535)
     {
         return 16;
     }
+    // 32 bits needed for numbers outside above ranges.
     else
     {
         return 32;
@@ -19,15 +23,32 @@ int calculateBitsNeeded(unsigned int number)
 
 int main(int argc, char *argv[])
 {
-    unsigned int num = atoi(argv[1]); // convert argument number to int.
+    if (argc != 2) // if 2 arguments are not provided
+    {
+        printf("Error, invalid number of arguments");
+        return 2; // Return error code 2 indicating unsuccessful conversion.
+    }
 
-    int bitNeeded = calculateBitsNeeded(num);
+    char *input = argv[1]; // Stores the input as a string;
+
+    for (int i = 0; input[i] != '\0'; i++) // loop over input
+    {
+        if (!isdigit(input[i])) // check if the value at the current position is not a number
+        {
+            printf("Error: Input is not a valid decimal number\n");
+            return 2;
+        }
+    }
+
+    unsigned int num = atoi(input); // convert argument number to int.
+
+    int bitNeeded = calculateBitsNeeded(num); // Calculate the number of bits needed
 
     char binaryStr[bitNeeded + 1]; // Array to hold the binaray representation answer bits + eventual null
 
     int index = 0; // index tracker for binaryStr array
 
-    unsigned int mask = 1 << (bitNeeded - 1);
+    unsigned int mask = 1 << (bitNeeded - 1); // Initialize mask for bitwise operations
 
     for (int i = 0; i < bitNeeded; i++)
     {
